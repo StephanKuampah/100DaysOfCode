a4dfbe91681d5ec708e07caf5d99dc791f2fd7a1
StephKuampah <Kuampahstephan>
2024-02-14 14:21:50 +0000

    Day_4

---
+++ Day 4/day4_challenge1.py
@@ -0,0 +1,9 @@
+import random
+
+outcome = random.randint(0,1)
+
+if outcome == 0 :
+    print("Heads")
+
+else:
+    print("Tails")
---
+++ Day 4/lists.py
@@ -0,0 +1,4 @@
+states_of_america = ["Iowa","Idaho","Delaware","New Jersey"]
+
+states_of_america.extend(["AAAAA","BBBBBBBB"])
+print(states_of_america)
