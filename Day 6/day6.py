c304c31de878c724edb0983c16f4389438a906d4
StephKuampah <Kuampahstephan>
2024-03-19 22:25:05 +0000

    Day 6 code lines used

---
+++ Day 6/day6.py
@@ -0,0 +1,34 @@
+turn_left =print("123")
+move =turn_left =print("123")
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+
+
+
+
+def movement():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+movement()
+
+for jump in range(6):
+    movement()
