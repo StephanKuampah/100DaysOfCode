7e818f68f20eff70c0f596a013f92f7582e6da71
StephKuampah <Kuampahstephan>
2024-03-20 02:23:47 +0000

    Hangman art added and game working

--- Day 7/day7_challenge1.py
+++ Day 7/day7_challenge1.py
@@ -1,5 +1,63 @@
 import random
 
+
+# Hangman Stages
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 # list of words to choose from
 words_list = ["practice", "python", "camel", "baboon", "ghana"]
 
@@ -11,10 +69,14 @@ select_word = random.randint(0, word_list_number-1)
 
 # selected word
 selected_word = words_list[select_word]
-print(selected_word)
+
 
 # selected word length for check in loop
 word_length = len(selected_word)
+
+
+# lives left
+lives = 6
 
 # Chosen word list 
 word = []
@@ -30,15 +92,26 @@ end_of_game = False
 # loop for continuous game till game over 
 while not end_of_game:
     # asking user for letter guess to start the game
-    guess = input("To start, Guess a letter:\n").lower()
+    guess = input("Guess a letter:\n").lower()
 
     #loop check
     for position in range(word_length):
         letter = selected_word[position]
         if letter == guess :
             word[position] = letter
+        
+    if guess not in selected_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            
+            print("Gameover, You got hanged.")
+            print("You lose!!!")
+        else:
+            print(stages[lives])
     print(word)
 
     if '_' not in word:
         end_of_game = True
         print("You Win")
+print(stages[lives])
