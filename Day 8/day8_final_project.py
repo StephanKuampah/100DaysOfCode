2c61aec8e52fbf89e8a8a9ab7051c08b996ccbed
StephKuampah <Kuampahstephan>
2024-03-23 12:26:31 +0000

    Challenge_done_and_personal_modifications_done

--- Day 8/day8_final_project.py
+++ Day 8/day8_final_project.py
@@ -1,27 +1,40 @@
-def encrypt(text, shift):
+def caesar(direction,text, shift):
+    continue_cipher = True
     cipher_text = ""
-    for letter in text:
-        position = alphabet.index(letter)
-        new_position = position + shift
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f'The encoded text is {cipher_text}')
-
-def decrypt(cipher_text, shift):
-    decrypt_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift
-        new_letter = alphabet[new_position]
-        decrypt_text += new_letter
-    print(f'The decoded text is {decrypt_text}')
+    if direction == "decode":
+        shift *= -1   
+    for char in text:
+        shift %= 26
+        if (char in alphabet and direction != "decode" and direction != "encode"):
+            continue_cipher = False
+        elif char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        else:
+            cipher_text += char
+    if continue_cipher == True:
+        print(f"The {direction}d text is, {cipher_text}")
+    else:
+        print("you have entered a wrong keyword")
+        print("The keyword is 'encode' to encrypt and 'decode' to decrypt:\n") 
+        
+    
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+from caeser_cipher import logo
+print(logo)
+print("Welcome to caesar cipher")
 
-if direction == "encode":
-    encrypt(text = text,shift = shift)
-else:
-    decrypt(cipher_text = text,shift = shift)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(direction = direction, text = text, shift = shift)
+
+    result = input("Type 'yes' try again or type 'no' to exit\n")
+    if result == 'no':
+        should_continue = False
+        print('Goodbye, Come again.')
