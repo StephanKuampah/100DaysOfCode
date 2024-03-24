80fb5304f80a79885d7a1cf18ee800ac054aa3e7
StephKuampah <Kuampahstephan>
2024-03-24 00:16:34 +0000

    figured_it_out_before_bed_lol

--- Day 9/day9_final_project.py
+++ Day 9/day9_final_project.py
@@ -11,26 +11,41 @@ from art import logo
 print(logo)
 
 bid_list = []
-highest_bid = 0
-highest_bidder = None
+
+
 
 def make_bid(name,bid):
     new_bid = {}
     new_bid["name"] = name
     new_bid["bid"] = bid
     bid_list.append(new_bid)
-    print(bid_list)
+
+
+should_continue = True
+while should_continue:
+
+    highest_bid = 0
+    highest_bidder = None
     for bid in bid_list:
         bid_value = bid['bid'] 
         if highest_bid < bid_value:
             highest_bid = bid_value
-            highest_bidder = highest_bid['name']
-    print(f"{highest_bidder} is the highest bidder with {highest_bid}")
+            highest_bidder = bid['name']
+        
+    name = input("What is your name?\n")
+    bid = int(input(f"What is your bid?\n$"))
+    make_bid(name=name, bid=bid)
+    result = input("Are there any other bidders? 'yes' to bid again or 'no' to exit\n")
+    if result == 'yes':
+        clear_terminal()
+
+    else:
+        should_continue = False
+        clear_terminal()
+        print(f"{highest_bidder} is the highest bidder with ${highest_bid}")
 
 
 
+        
+    
 
-
-name = input("What is your name?\n")
-bid = int(input(f"What is your bid?\n$"))
-make_bid(name=name, bid=bid)
