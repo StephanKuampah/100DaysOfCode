81e36bd95c307a236220ec1d7def1571550208c7
StephKuampah <Kuampahstephan>
2024-06-08 09:19:05 +0000

    errors_corrected_blackjack_finished

--- Day 11/art.py
+++ Day 11/art.py
@@ -7,4 +7,4 @@ logo = '''
 88       d8 88 ,adPPPPP88 8b         8888[     88 ,adPPPPP88 8b         8888[      
 88b,   ,a8" 88 88,    ,88 "8a,   ,aa 88`"Yba,  88 88,    ,88 "8a,   ,aa 88`"Yba,   
 8Y"Ybbd8"'  88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a 88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a  
-                                              ,88                                  '''
+                                              ,88                                 '''
--- Day 11/day11_blackjack.py
+++ Day 11/day11_blackjack.py
@@ -22,6 +22,13 @@ def draw():
 # List of cards
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+def ace_check():
+    # If sum of cards is greater than 21 and there's an ace (11), consider it as 1
+        if sum(cards) > 21 and 11 in cards:
+            cards.remove(11)
+            cards.append(1)
+        return sum(cards)
+
 
 should_continue = True
 while should_continue:
@@ -32,13 +39,14 @@ while should_continue:
     start_game = input("Do you want to play blackjack? 'yes' or 'no'\n")
     
     if start_game.lower() == 'yes':
-        clear_terminal()  # Moved to after the input prompt
+        clear_terminal() 
         player_cards = draw2()
         computer_cards = draw2()
         print(logo)
         print(f"Your cards: {player_cards}")
         print(f"Computer's first card is {computer_cards[0]}")
 
+       
         # Player's turn
         while sum(player_cards) < 21:
             to_continue = input("Do you want to draw another card? 'yes' to draw again or 'no' to get results\n")
@@ -50,9 +58,12 @@ while should_continue:
                 break
 
         # Computer's turn
-        while sum(computer_cards) < 17:
+        while sum(computer_cards) < 17 and sum(player_cards) <= 21:
             new_card = draw()
-            computer_cards.append(new_card)     
+            computer_cards.append(new_card)    
+
+        ace_check() 
+
 
         # Final cards
         print(f"Your final cards are {player_cards}")
@@ -70,18 +81,20 @@ while should_continue:
                 print("You win")
         elif computer_score > 21:
             print("You win")
-        elif player_score > computer_score:
+        elif player_score > computer_score and player_score <= 21 and computer_score <= 21:
             print("You win")
-        elif player_score < computer_score:
+        elif player_score < computer_score and player_score <= 21 and computer_score <= 21:
             print("You lose")
         else:
             print("It's a draw")
 
         # Ask the user if they want to play again
         play_again = input("Do you want to play again? 'yes' or 'no'\n")
-        if play_again.lower() != 'yes':
-            should_continue = False
-            print("Thank you for playing!")
+        if play_again == 'yes':
+            clear_terminal()
+        else:
+            print('Thank You for playing')
+       
     else:
         should_continue = False
         print("You did not start the game")
