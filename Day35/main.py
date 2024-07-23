e5d1cfe99feceb7cfd7c249d28add322e6f2c09c
StephKuampah <Kuampahstephan>
2024-07-23 16:38:46 +0000

    Day35

--- Day35/main.py
+++ Day35/main.py
@@ -1,3 +1,4 @@
+from multiprocessing.connection import Client
 import requests
 from twilio.rest import client
 
@@ -6,6 +7,8 @@ from twilio.rest import client
 # endpoint =  "https://api.openweathermap.org/data/2.5/weather"
 endpoint  = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "a520f611de09cd24bf77a18f43de1524"
+account_sid = "sample_id"
+auth_token = "sample_token"
 
 weather_params = {
     "lat": 51.507351,
@@ -26,5 +29,11 @@ for hour_data in data_slice:
     if int(condition) < 700:
         will_rain = True
 if will_rain:
-    print("Bring an umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(body = "It's going to rain today. Remeber to bring an Umbrella",
+            from_= "+233500000000",
+            to= "+233500000001"
+        )
+    print(message.status)
 
